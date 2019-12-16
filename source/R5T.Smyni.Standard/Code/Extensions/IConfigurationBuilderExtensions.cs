0bcb3a6222a9b962cf60de0b9c84f8596292ce20
MinexAutomation <minex.automation@gmail.com>
2019-12-16 11:13:16 -0800

    Added configuration-builder and service-collection extensions for the Visual Studio file operator.

---
+++ source/R5T.Smyni.Standard/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Norsica.Standard;
+
+
+namespace R5T.Smyni.Standard
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddVisualStudioFileOperatorConfiguration(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
+        {
+            configurationBuilder.AddDotnetConfiguration(configurationServiceProvider);
+
+            return configurationBuilder;
+        }
+    }
+}
---
+++ source/R5T.Smyni.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Norsica.Standard;
+using R5T.Smyni.Norsica;
+
+
+namespace R5T.Smyni.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddVisualStudioFileOperator(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileOperator, DotnetVisualStudioSolutionFileOperator>()
+                .AddDotnetOperator()
+                ;
+
+            return services;
+        }
+    }
+}
