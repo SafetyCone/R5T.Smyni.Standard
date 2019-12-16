8b2fa8b9a060a40e23eabfe969ca13c6beef3159
MinexAutomation <minex.automation@gmail.com>
2019-12-16 11:02:32 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Smyni.Standard.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Smyni.Standard.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Smyni.Standard/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Smyni.Standard
+{
+    public class Class1
+    {
+    }
+}
